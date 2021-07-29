663029d66557efdae8fb705c924b11bd5d770d49
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2021-07-30 00:35:46 +0530

    Create gen_all_binary_arrays_n_length.cpp

---
+++ backtracking/gen_all_binary_arrays_n_length.cpp
@@ -0,0 +1,33 @@
+// generate all 'n' length strings (consisting of 0's & 1's) using backtracking
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<char> str;
+
+void generate(int n){
+    if(n < 0){
+        for(auto i : str){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    else{
+        str[n] = '0';
+        generate(n-1);
+        str[n] = '1';
+        generate(n-1);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    str.resize(n);
+    
+    generate(n);
+    
+    return 0;
+    
+}
