c470ab0127a293e0f472fa83e3a43c642cfd6c48
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2021-08-26 11:30:29 +0530

    Create chewbacca.cpp

---
+++ Array/chewbacca.cpp
@@ -0,0 +1,58 @@
+Problem Name: Chewbacca and Number
+Problem Difficulty: 1
+Problem Constraints: x <= 100000000000000000
+Problem Description:
+Luke Skywalker gave Chewbacca an integer number x. Chewbacca isn't good at numbers but he loves inverting digits in them. Inverting digit t means replacing it with digit 9 - t.
+
+Help Chewbacca to transform the initial number x to the minimum possible positive number by inverting some (possibly, zero) digits. The decimal representation of the final number shouldn't start with a zero.
+Input Format: The first line contains a single integer x (1 ≤ x ≤ 10^18) — the number that Luke Skywalker gave to Chewbacca.
+Sample Input: 4545
+Output Format: Print the minimum possible positive number that Chewbacca can obtain after inverting some digits. The number shouldn't contain leading zeroes.
+Sample Output: 4444
+
+
+
+=====Solution=====
+import java.util.*;
+import java.lang.Math;
+
+public class Chewbaaca_num{
+
+
+  public static void main(String[] args){
+
+    Scanner scn = new Scanner(System.in);
+
+        long n = scn.nextLong();
+
+       System.out.println(smallest_num(n));
+    }
+  
+  public static long smallest_num(long num){
+
+
+    long ans = 0;
+    long mult = 1;
+
+    while(num != 0){
+
+      long rem = num % 10;
+
+      if(rem >= 5){
+
+        if(rem == 9 && (num/10) == 0){
+         //do nothing
+        }else{
+          rem = 9 - rem;
+        }
+      }
+
+      ans += rem * mult;
+
+      mult *= 10;
+      num /= 10;
+    }
+
+    return ans;
+  }
+}
